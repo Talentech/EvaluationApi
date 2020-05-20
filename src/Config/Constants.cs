6f8db22bb87130bf289f1bb1aed08d306c27b8d8
ThisGuy89 <martin.gleditsch@talentech.com>
2020-05-20 14:43:14 +0200

    Add authorize+token endpoint

--- src/Config/Constants.cs
+++ src/Config/Constants.cs
@@ -7,6 +7,8 @@
             public const string SendInvitationEndpoint = "invitations";
             public const string GetEvaluationFormsEndpoint = "evaluationforms";
             public const string HealthCheckEndpoint = "health";
+            public const string AuthorizationEndpoint = "authorization/authorize";
+            public const string TokenEndpoint = "authorization/token";
             public const string DeauthorizationEndpoint = "authorization/deauthorize";
         }
     }
