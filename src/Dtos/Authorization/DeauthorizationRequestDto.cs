8a43897f948235b41d2c7c53485002e1e93490ed
Martin <martin.gleditsch@talentech.com>
2020-06-09 14:35:33 +0200

    Added ClientId and ClientSecret to the DeauthorizationRequestDto

--- src/Dtos/Authorization/DeauthorizationRequestDto.cs
+++ src/Dtos/Authorization/DeauthorizationRequestDto.cs
@@ -8,9 +8,20 @@ namespace Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Authorization
         /// Information about the ATS user who executes the request
         /// </summary>
         public TriggeredByDto TriggeredBy { get; set; }
+
         /// <summary>
-        /// The schema for this is controlled by the Partner. The contents here is identical to what is returned from the OAuth token endpoint.
+        /// The schema for this is controlled by the partner. The contents here is identical to what is returned from the OAuth token endpoint.
         /// </summary>
         public ExamplePartnerTokenDto Auth { get; set; }
+
+        /// <summary>
+        /// The client id given to Talentech by the partner
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// The client secret given to Talentech by the partner
+        /// </summary>
+        public string ClientSecret { get; set; }
     }
 }
