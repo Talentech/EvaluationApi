b9d3f783d2afe576ca3f84905be144c8327999d3
Martin <martin.gleditsch@talentech.com>
2021-09-15 13:53:31 +0200

    Added a sample error body

--- src/Dtos/Common/ErrorDto.cs
+++ src/Dtos/Common/ErrorDto.cs
@@ -1,8 +1,19 @@
-﻿namespace Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Common
+﻿using System.Collections.Generic;
+
+namespace Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Common
 {
     public class ErrorDto
     {
         public ErrorType ErrorType { get; set; }
         public string Message { get; set; }
+
+        public static List<ErrorDto> SampleErrorResponseBody()
+        {
+            return new List<ErrorDto>
+            {
+                new ErrorDto { ErrorType = ErrorType.SystemError, Message = "An technical error message of your choice that won't be shown to end users" },
+                new ErrorDto { ErrorType = ErrorType.UserError, Message = "An end user friendly error message" }
+            };
+        }
     }
 }
