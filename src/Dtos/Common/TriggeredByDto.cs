c3bdfd70744119ea410c76949575e8bd3512d9f3
Michael Eikeland <michael.eikeland@talentech.com>
2022-01-14 15:13:46 +0100

    Update reference implementation with new fields

--- src/Dtos/Common/TriggeredByDto.cs
+++ src/Dtos/Common/TriggeredByDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Common
+﻿using System.Collections.Generic;
+
+namespace Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Common
 {
     public class TriggeredByDto
     {
+        public IEnumerable<string> EncryptedFields { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- src/Dtos/Results/ResultUriDto.cs
+++ src/Dtos/Results/ResultUriDto.cs
@@ -1,9 +1,11 @@
-﻿using Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Common;
+﻿using System.Collections.Generic;
+using Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Common;
 
 namespace Talentech.EvaluationApi.SamplePartnerApiConnector.Dtos.Results
 {
     public class ResultUriDto
     {
+        public IEnumerable<string> EncryptedFields { get; set; }
         public string Name { get; set; }
         public UriType Type { get; set; }
         public string Uri { get; set; }
